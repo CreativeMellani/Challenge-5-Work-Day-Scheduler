292ee65b8b801de370deb33e3e5e15ce1f42c680
CreativeMellani <101590432+CreativeMellani@users.noreply.github.com>
2022-06-09 21:24:39 -0700

    Updated Script

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,16 @@
 // GIVEN I am using a daily planner to create a schedule
+
 // WHEN I open the planner// THEN the current day is displayed at the top of the calendar
+// referenced for help: https://stackoverflow.com/questions/25734743/how-to-return-the-current-timestamp-with-moment-js
+var jun = function moment("2022-06-05T12:00:00Z");
+console.log (currentTime)
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
 
 // WHEN I click into a timeblock
 // THEN I can enter an event
