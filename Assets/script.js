5709bb5ccdba054aafc620501d7e398da040266b
CreativeMellani <101590432+CreativeMellani@users.noreply.github.com>
2022-06-10 16:24:59 -0700

    Added current time in the header of the schedule.

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,23 @@
 // GIVEN I am using a daily planner to create a schedule
 
 // WHEN I open the planner// THEN the current day is displayed at the top of the calendar
-// referenced for help: https://stackoverflow.com/questions/25734743/how-to-return-the-current-timestamp-with-moment-js
-var jun = function moment("2022-06-05T12:00:00Z");
-console.log (currentTime)
+// referenced for help: https://stackoverflow.com/questions/25734743/how-to-return-the-current-timestamp-with-moment-js and https://www.youtube.com/watch?v=o0lMOtYYB_E
+
+// var jun = function moment("2022-06-05T12:00:00Z");
+// console.log (currentTime)
+
+//Get value
+let clock = document.getElementById("clock");
+
+//Set Time
+ setInterval(function() {
+     let date = new Date();
+     clock.innerHTML = date.toLocaleTimeString(); //Method
+
+ }, 1000);
+
+
+
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
