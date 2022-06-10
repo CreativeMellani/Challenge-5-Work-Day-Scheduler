86ba1318f54ce325d95c088e7f5434c3e3c39c00
CreativeMellani <101590432+CreativeMellani@users.noreply.github.com>
2022-06-10 16:47:12 -0700

    Added input lines to each time block.

--- Assets/script.js
+++ Assets/script.js
@@ -17,20 +17,17 @@ let clock = document.getElementById("clock");
  }, 1000);
 
 
-
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
 
-// WHEN I click into a timeblock
-// THEN I can enter an event
+
 
 // WHEN I click the save button for that timeblock
 // THEN the text for that event is saved in local storage
+// reference this video for help save user input src: https://www.youtube.com/watch?v=w9GNyw8AvlM 
+
+
 
 // WHEN I refresh the page
 // THEN the saved events persist
